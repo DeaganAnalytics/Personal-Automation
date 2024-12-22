8bb14ff16db4994b76e8ad81b52c10dfae3a6051
DeaganAnalytics <61816088+DeaganAnalytics@users.noreply.github.com>
2024-12-23 10:22:47 +1030

    Add files via upload

---
+++ R Scripts/standard_dates.R
@@ -0,0 +1,16 @@
+# This file calculates the end of the previous month, quarter, half year and year, based on today's date
+
+# Previous Month End
+pm_end <- as.character(floor_date(Sys.Date(), "month") - 1)
+
+# Previous Month End Lagged
+pm_end_lagged <- as.character(floor_date((floor_date(Sys.Date(), "month") - 1), "month") - 1)
+
+# Previous Quarter End
+pq_end <- as.character(floor_date(Sys.Date(), "quarter") - 1)
+
+# Previous Half Year End
+ph_end <- as.character(floor_date(Sys.Date(), "halfyear") - 1)
+
+# Previous Year End
+py_end <- as.character(floor_date(Sys.Date(), "year") - 1)
