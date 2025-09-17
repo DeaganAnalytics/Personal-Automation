3d1c9207a9fd612042e0f5d1a35446e36c96938f
Seamus Deagan <61816088+DeaganAnalytics@users.noreply.github.com>
2025-09-17 13:22:21 +0930

    Update server_connection.R
    Generalise variable name

--- R/Functions/server_connection.R
+++ R/Functions/server_connection.R
@@ -1,7 +1,7 @@
 library("odbc")
 
 # Connect to databases
-con_edw <- DBI::dbConnect(odbc::odbc(),
+con_name <- DBI::dbConnect(odbc::odbc(),
                           Driver = "", # Insert driver (i.e. "SQL Server")
                           Server = "", # Insert server name
                           Database = "", # Insert database name
